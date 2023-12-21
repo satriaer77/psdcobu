0ba757ac8a89f45ff4f95629e672f78c9a98d703
satriaer77 <ppicbimasatria@gmail.com>
2023-12-21 08:18:03 +0700

    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_absolute_percentage_error as maep
 
 from sklearn.linear_model import LinearRegression
 
-#from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 from IPython.display import Image
 import IPython
 import seaborn as sns
